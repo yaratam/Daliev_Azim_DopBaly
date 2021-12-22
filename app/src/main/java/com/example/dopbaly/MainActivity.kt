a28f5e06bfc3236576ee30578a9ee8517c377b51
dalie <darknight0417@gmail.vom>
2021-12-22 12:21:02 +0600

    Initial commit

---
+++ app/src/main/java/com/example/dopbaly/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dopbaly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
